63e8a47400dd2afb8824cd4d4399645b2f1f6c69
Alice Lopes <lopesalice4745@gmail.com>
2023-11-23 23:23:38 -0300

    fixing some stuff

--- geral/models.py
+++ geral/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class produto(models.Model):
     nome = models.CharField(max_length=30)
-    preco = models.FloatField()
-    quantidade = models.IntegerField()
+    preco = models.CharField(max_length=30)
+    quantidade = models.CharField(max_length=30)
 
 
 
